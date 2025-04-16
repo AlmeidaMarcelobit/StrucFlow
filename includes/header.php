c467a28694dff02a1d23b697258eeaac10285604
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-16 17:21:41 -0300

    ✨ feat: adiciona novo arquivo de cabeçalho com navegação principal
    Inclui um novo arquivo `header.php` que contém a estrutura de navegação
    principal do site. Este cabeçalho melhora a usabilidade ao fornecer
    links diretos para as principais seções, como Colaboradores, Centro de
    Custo, Monitoramento Parque e Estoque. A inclusão de menus suspensos
    para Colaboradores e Estoque permite uma navegação mais organizada e
    acessível, melhorando a experiência do usuário.

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<header>
+    <nav class="nav" aria-label="Main navigation">
+        <ul>
+            <li><a href="../includes/index.php">Home</a></li>
+
+            <li class="drop" aria-haspopup="true" aria-expanded="false">
+                <a href="#">Colaboradores ▾</a>
+                <ul class="dropdown" aria-label="Submenu">
+                    <li><a href="../colaboradores/Secao%20A.php">Seção A</a></li>
+                    <li><a href="../colaboradores/Secao%20B.php">Seção B</a></li>
+                    <li><a href="../colaboradores/Secao%20C.php">Seção C</a></li>
+                </ul>
+            </li>
+
+            <li><a href="#">Centro de Custo</a></li>
+            <li><a href="../pages/monitoramento-park.php">Monitoramento Parque</a></li>
+
+            <li class="drop" aria-haspopup="true" aria-expanded="false">
+                <a href="#">Estoque ▾</a>
+                <ul class="dropdown" aria-label="Submenu">
+                    <li><a href="../pages/celular.php">Celular</a></li>
+                    <li><a href="../pages/fone.php">Fones</a></li>
+                    <li><a href="../pages/kits.php">Kits</a></li>
+                    <li><a href="../pages/monitor.php">Monitor</a></li>
+                    <li><a href="../pages/notebook.php">Notebooks</a></li>
+                    <li><a href="../pages/suporte.php">Suporte</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
